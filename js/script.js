15f4bcfdae08f901963ded92287abb25862f6493
Soro Cat <sorousha326@gmail.com>
2019-10-02 11:26:40 -0700

    add changes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
-//business logic for moNey
-function Audience() {
-  this.BankAccount = [],
+//business logic for Money
+function Money() {
+  this.bankAccount = [],
   this.currentId = 0
   }
 
-//business logic for BankAccount
+Money.prototype.newbankAccount = function(bankAccount) {
+  bankAccount.id = this.assignId();
+  this.bankAccount.push(bankAccount);
+}
+
+Money.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+//business logic for Bankaccount
 
 // User Interface Logic
+var money = new Money();
+
 $(document).ready(function() {
   $("form#bankrupt").submit(function(event) {
     event.preventDefault()
