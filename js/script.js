35b94c57f6eb18d664d0d1c6c932417114a42eae
Soro Cat <sorousha326@gmail.com>
2019-10-02 10:01:44 -0700

    add js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//business logic for moNey
+
+//business logic for BankAccount
+
+// User Interface Logic
+$(document).ready(function() {
+  $("form#bankrupt").submit(function(event) {
+    event.preventDefault()
